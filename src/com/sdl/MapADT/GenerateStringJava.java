2a72e96b8e6ba990cb15bbd41bc9af6d3fb9ee12
Gabriel Simon Mbaiorga <gabrielmbaiorga@gmail.com>
2022-09-04 22:48:38 +0100

    generate String using RandomUtils

--- src/com/sdl/MapADT/GenerateStringJava.java
+++ src/com/sdl/MapADT/GenerateStringJava.java
@@ -3,12 +3,14 @@ package com.sdl.MapADT;
 import org.apache.commons.lang.RandomStringUtils;
 
 import java.security.SecureRandom;
+import java.util.Arrays;
 import java.util.UUID;
 
 public class GenerateStringJava {
     public static void main(String[] args){
         System.out.println(generateString1(32));
         System.out.println("Generate using UUID: "+ generateStringFromUUID());
+        System.out.println(Arrays.toString(squareNumbers(10)));
     }
 
     public static String generateString1(int num){
@@ -17,6 +19,15 @@ public class GenerateStringJava {
 
     public static String generateStringFromUUID(){
         return UUID.randomUUID().toString().replace("-","");
+    }
+
+    public static int[] squareNumbers(int num){
+        int[] nums = new int[num];
+        for (int i = 0; i < num; i++){
+            int square = (i + 1)* (i + 1);
+            nums[i] =square;
+        }
+        return nums;
     }
 
 
