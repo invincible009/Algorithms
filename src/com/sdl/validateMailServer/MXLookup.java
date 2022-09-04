8809b357ee4cc6abbaeda9f1fb78fbbe598e7ff7
Gabriel Simon Mbaiorga <gabrielmbaiorga@gmail.com>
2022-09-04 23:23:36 +0100

    mail lookup

---
+++ src/com/sdl/validateMailServer/MXLookup.java
@@ -0,0 +1,13 @@
+package com.sdl.validateMailServer;
+
+
+/**
+ * The best you can do to verify if an email address is real is to verify
+ * if there is a mail server registered to the domain name.
+ */
+public class MXLookup {
+
+    public static void main(String[] args) {
+
+    }
+}
