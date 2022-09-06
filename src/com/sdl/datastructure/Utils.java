24fdcdf77f859630b0661ca850ad7ddb22cdd408
Gabriel Simon Mbaiorga <gabrielmbaiorga@gmail.com>
2022-09-06 23:01:45 +0100

    sort utility

---
+++ src/com/sdl/datastructure/Utils.java
@@ -0,0 +1,15 @@
+package com.sdl.datastructure;
+
+public class Utils {
+
+    public static void swap(int[] array, int i, int j){
+
+        if(i == j){
+            return;
+        }
+
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+}
