d740cadb82e7200f03994f15c72243ee2ae01233
gabby_dev <gabrielmbaiorga@gmail.com>
2023-02-08 18:15:51 +0100

    add assigment for quicksort

--- AlgorithDeepDive/src/main/java/org/example/countingSort/CountingSortImpl.java
+++ AlgorithDeepDive/src/main/java/org/example/countingSort/CountingSortImpl.java
@@ -4,7 +4,8 @@ public class CountingSortImpl {
     public static void main(String[] args) {
 //        int[] intArray ={20, 23,55,54,12,34,67,89,98,76,90,10,2,8,5,6,3};
         int[] intArray ={2, 5,9,8,2,8,7,10,4,3};
-        countingSort(intArray, 1, 10);
+        //countingSort(intArray, 1, 10);
+        countingSortWithoutArrMinMax(intArray, intArray.length);
         for(int num : intArray){
             System.out.print(num+ ", ");
         }
@@ -26,6 +27,47 @@ public class CountingSortImpl {
                 inputArr[indexUseToWriteToInputArray++]= indexUsedToTraverseArr;
                 countArray[indexUsedToTraverseArr - min]--;
             }
+        }
+    }
+
+    //if you are given the array without a min and max index but the size of the array
+    public static void countingSortWithoutArrMinMax(int[] array, int size){
+
+        int[] output = new int[size + 1];
+
+        // Find the largest element of the array
+        int max = array[0];
+        for (int i = 1; i < size; i++) {
+            if (array[i] > max)
+                max = array[i];
+        }
+        int[] count = new int[max + 1];
+
+        // Initialize count array with all zeros.
+        for (int i = 0; i < max; ++i) {
+            count[i] = 0;
+        }
+
+        // Store the count of each element
+        for (int i = 0; i < size; i++) {
+            count[array[i]]++;
+        }
+
+        // Store the cummulative count of each array
+        for (int i = 1; i <= max; i++) {
+            count[i] += count[i - 1];
+        }
+
+        // Find the index of each element of the original array in count array, and
+        // place the elements in output array
+        for (int i = size - 1; i >= 0; i--) {
+            output[count[array[i]] - 1] = array[i];
+            count[array[i]]--;
+        }
+
+        // Copy the sorted elements into original array
+        for (int i = 0; i < size; i++) {
+            array[i] = output[i];
         }
     }
 }
