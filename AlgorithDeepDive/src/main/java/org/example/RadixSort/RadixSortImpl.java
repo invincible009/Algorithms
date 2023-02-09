c32f7e541a6653c00a3ed85657b91f18dc4bbfa7
gabby_dev <gabrielmbaiorga@gmail.com>
2023-02-09 17:12:09 +0100

    add radix sort background

---
+++ AlgorithDeepDive/src/main/java/org/example/RadixSort/RadixSortImpl.java
@@ -0,0 +1,12 @@
+package org.example.RadixSort;
+
+public class RadixSortImpl {
+    public static void main(String[] args) {
+        int[] intArray ={20, 23,55,-54,12,-34,67,89,98,76,90,10,2,8,-5,6,-3};
+//        quickSort(intArray, 0, intArray.length);
+        for(int num : intArray){
+            System.out.print(num+ ", ");
+        }
+    }
+
+}
