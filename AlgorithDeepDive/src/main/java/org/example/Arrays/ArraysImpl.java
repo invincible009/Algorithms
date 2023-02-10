f2258534c82c62deadcbc2ced44ca400148ae0ba
gabby_dev <gabrielmbaiorga@gmail.com>
2023-02-10 17:55:25 +0100

    radix sort implementation

---
+++ AlgorithDeepDive/src/main/java/org/example/Arrays/ArraysImpl.java
@@ -0,0 +1,6 @@
+package org.example.Arrays;
+
+import java.util.Arrays;
+
+public class ArraysImpl {
+}
--- AlgorithDeepDive/src/main/java/org/example/Main.java
+++ AlgorithDeepDive/src/main/java/org/example/Main.java
@@ -1,7 +1,13 @@
 package org.example;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] intArray ={20, 23,55,-54,12,-34,67,89,98,76,90,10,2,8,-5,6,-3};
+       Arrays.parallelSort(intArray);
+        for(int num : intArray){
+            System.out.print(num+ ", ");
+        }
     }
 }
